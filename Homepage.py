72b3020577225a6a32008c667ac0ad2ef52343d0
Ram <a.ramachandran.2002@gmail.com>
2023-06-21 22:26:47 +0530

    Updated App

---
+++ Homepage.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Multipage App",
+    page_icon="👋",
+)
+
+st.title("Main Page")
+st.sidebar.success("Select a page above.")
+st.write("""### Welcome to Movie Gallary, we provide the following services :
+##### 1. Netflix Movie Recommendation 
+     Contains the latest netflix movies
+##### 2. Movie Selection and choice! 
+     Contains different movies which exist currently
+     
+     
+##### 👈 Pick your choice from the side menu present """)
+
---
+++ pages/4_📞_Contact.py
@@ -0,0 +1,14 @@
+import streamlit as st
+st.title("Contact")
+st.write("""
+Here is my contact details , for any queries 
+##### Mail : 
+    a.ramachandran.2002@gmail.com
+##### GitHub : 
+    https://github.com/Ram1103
+##### LinkedIn : 
+    https://www.linkedin.com/in/a-ramachandran/   
+    
+    
+    
+""")
